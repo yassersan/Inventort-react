64fead4562c236335b09bc2369818154c4fe7e7a
yassersa <yaser.snober@gmail.com>
2023-07-24 15:17:13 -0700

    linking front-end

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ItemForm from './itemForm';
 import ItemList from './itemList';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("https://inventory-manager-im94.onrender.com")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  },[]);
+
   return (
     <div>
       <h1>Item List</h1>
